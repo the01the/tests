07028d47f7e2e8c964148427f0373babc0449a8e
the01the <star.arrows.star2@gmail.com>
2025-05-07 06:02:00 +0900

    commit timmer event

--- tests/MainWindow.xaml.cs
+++ tests/MainWindow.xaml.cs
@@ -9,6 +9,13 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
+//timer
+using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Threading;
+
 namespace tests
 {
     /// <summary>
@@ -19,6 +26,42 @@ namespace tests
         public MainWindow()
         {
             InitializeComponent();
+
+            SetupTimer();// タイマーセットアップ
+
+        }
+
+        string eventtext = "";
+
+        // タイマメソッド
+        private void MyTimerMethod(object sender, EventArgs e)
+        {
+            this.TextBlock1.Text = $"{DateTime.Now.ToString("HH:mm:ss")}{eventtext}";
+        }
+
+        // タイマのインスタンス
+        private DispatcherTimer _timer;
+
+        // タイマを設定する
+        private void SetupTimer()
+        {
+            // タイマのインスタンスを生成
+            _timer = new DispatcherTimer(); // 優先度はDispatcherPriority.Background
+            // インターバルを設定
+            _timer.Interval = new TimeSpan(0, 0, 30);
+            // タイマメソッドを設定
+            _timer.Tick += new EventHandler(MyTimerMethod);
+            // タイマを開始
+            _timer.Start();
+
+            // 画面が閉じられるときに、タイマを停止
+            this.Closing += new CancelEventHandler(StopTimer);
+        }
+
+        // タイマを停止
+        private void StopTimer(object sender, CancelEventArgs e)
+        {
+            _timer.Stop();
         }
     }
 }
---
+++ tests/configurationAleart.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace tests
+{
+    /// <summary>
+    /// configurationAleart.xaml の相互作用ロジック
+    /// </summary>
+    public partial class configurationAleart : Window
+    {
+        List<string[]> ConfChangeList { get; set; }
+        public configurationAleart()
+        {
+            InitializeComponent();
+            ConfChangeList = new List<string[]>();
+            ConfChangeList.Add(new string[] { "DocNum", "ChangeNum", "->", "af_DocNum", "af_ChangeNum" });
+            ConfChangeList.Add(new string[] { "DocNum", "ChangeNum", "->", "af_DocNum", "af_ChangeNum" });
+            ConfChangeList.Add(new string[] { "DocNum", "ChangeNum", "->", "af_DocNum", "af_ChangeNum" });
+            ConfChangeGrid.ItemsSource = (System.Collections.IEnumerable)ConfChangeList;
+
+        }
+    }
+}
